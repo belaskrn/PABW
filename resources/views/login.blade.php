d167b96f11f13bd64b08f179a4f9113c3b81fc56
orcaja99 <tealatte8@gmail.com>
2023-03-05 22:07:47 +0800

    css working now

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,13 @@
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>login form</title>
-
-   <!-- custom css file link  -->
-   <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet" type="text/css" >
-
+   <link href="/css/app.css" rel="stylesheet" type="text/css" >
 </head>
 <body>
    
 <div class="form-container">
 
    <form action="" method="post">
-      <h3>login now</h3>
+      <h3>login</h3>
       <?php
       if(isset($error)){
          foreach($error as $error){

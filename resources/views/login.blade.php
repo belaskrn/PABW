f4224521323b262a56aa3e9c5127a71a27c8f91b
orcaja99 <tealatte8@gmail.com>
2023-03-07 14:31:16 +0800

    databse

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,5 @@
 <head>
-   <title>login form</title>
+   <title>sign-in</title>
    <link href="/css/app.css" rel="stylesheet" type="text/css" >
 </head>
 <body>
@@ -7,7 +7,7 @@
 <div class="form-container">
 
    <form action="" method="post">
-      <h3>login</h3>
+      <h3>Sign in</h3>
       <?php
       if(isset($error)){
          foreach($error as $error){
@@ -15,10 +15,10 @@
          };
       };
       ?>
-      <input type="email" name="email" required placeholder="enter your email">
-      <input type="password" name="password" required placeholder="enter your password">
+      <input type="email" name="email" required placeholder="Masukkan email">
+      <input type="password" name="password" required placeholder="Masukkan password">
       <input type="submit" name="submit" value="login now" class="form-btn">
-      <p>don't have an account? <a href="register_form.php">register now</a></p>
+      <p>tidak punya akun? <a href="register_form.php">sign up</a></p>
    </form>
 
 </div>

0908f7e87935497576d13fb1565510a9f540777b
herliananur <herliananur19@gmail.com>
2023-03-26 21:59:47 +0800

    nambahin icon

--- resources/views/profileuser.blade.php
+++ resources/views/profileuser.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!-- <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -16,9 +16,66 @@
 
         <a class="navbar-brand" href="#">
           <img src="img/TicketIn.png" alt="TicketIn" width="120" height="26">
+        </a> -->
+        <!-- <button type="button" class="btn btn-success">Success</button> -->
+        <!-- <button class="btn btn-outline-success" type="submit">Keluar</button>
+
+      </div>
+    </nav>
+  </head>
+  <body> -->
+  
+    <!-- Example Code -->
+    
+    
+<!-- 
+    <div class="row">
+    <div class="col-sm-7 mb-3 mb-sm-0">
+            <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+              <button type="button" class="btn btn-primary"><img src="\img\p.png" class="rounded-circle" width="150">Nama yang punya akun</button>
+              <button type="button" class="btn btn-primary">TicketInPay</button>
+              <button type="button" class="btn btn-primary">Pesanan</button>
+              <button type="button" class="btn btn-primary">Riwayat Pemesanan</button>
+              <button type="button" class="btn btn-primary">Keluar Akun</button>
+            </div>
+        
+      </div>
+    </div> -->
+    
+    <!-- End Example Code -->
+  <!-- </body>
+  <footer class="text-white py-2">
+    <div class="container mt-1 d-flex justify-content-center">
+      <a class="navbar-brand" href="#">
+        <img src="img/TicketIn.png" alt="Logo" width="100" height="22" class="d-inline-block align-text-top">&#169; 2023, Kelompok 5 PABW B
+      </a>
+    </div>
+  </footer>
+</html> -->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/profileuserakunsaya.css">
+    <title>Profile User</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    
+    <nav class="navbar navbar-dark" data-bs-theme="dark">
+      <div class="container">
+
+        <a class="navbar-brand" href="#">
+          <img src="img/TicketIn.png" alt="TicketIn" width="120" height="26">
         </a>
         <!-- <button type="button" class="btn btn-success">Success</button> -->
-        <button class="btn btn-outline-success" type="submit">Keluar</button>
+        <button class="btn btn-light" type="submit">Keluar</button>
 
       </div>
     </nav>
@@ -35,11 +92,11 @@
           <div class="col-4">
             <div class="container text-center">
               <div class="row row-cols-1">
-                 <div class="col"><img src="\img\p.png" class="rounded-circle" width="150">Nama yang punya akun</div>
-                  <div class="col"><img src="..." class="img-thumbnail" alt="">TicketInPay</div>
-                  <div class="col">Pesanan</div>
-                  <div class="col">Riwayat Pemesanan</div>
-                  <div class="col">Keluar Akun</div>
+                 <div class="col"><img src="\img\p.png" class="rounded-circle" width="150">Fauzan Maftuh Alwafi</div>
+                  <div class="col"><ion-icon name="wallet-outline" size="small"></ion-icon>  TicketInPay</div>
+                  <div class="col"><ion-icon name="reader-outline" size="small"></ion-icon>  Pesanan</div>
+                  <div class="col"><ion-icon name="receipt-outline" size="small"></ion-icon>  Riwayat Pemesanan</div>
+                  <div class="col"><ion-icon name="log-out-outline" size="small"></ion-icon>  Keluar Akun</div>
               </div>
             </div>
           </div>

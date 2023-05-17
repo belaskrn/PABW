dc1ec4c44f6a3c1f664945724f81f64a7d72d56f
TrixTin23 <11201018@student.itk.ac.id>
2023-05-17 16:43:25 +0800

    new login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,25 +1,64 @@
 <head>
    <title>login</title>
-   <link href="/css/app.css" rel="stylesheet" type="text/css" >
+   <link href="/css/login.css" rel="stylesheet" type="text/css" >
+
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-   
-<div class="form-container">
-   <form action="{{ route('login-user') }}" method="POST">
-      @if(Session::has('sukses')):
-      <div class="alert alert success">{{ Session::get('sukses') }}</div>
-      @endif
-      @if(Session::has('gagal')):
-      <div class="alert alert success">{{ Session::get('gagal') }}</div>
-      @endif
-      @csrf
-      <h3>Sign in</h3>
-      <input type="email" name="email" required placeholder="Masukkan email">
-      <input type="password" name="password" required placeholder="Masukkan password">
-      <input type="submit" name="submit" value="login now" class="form-btn">
-      <p>Daftar Akun ke Nomor Berikut <a href="https://wa.me/+6283152582912">083152582912</a></p>
-   </form>
-</div>
+
+  <section class="vh-100">
+    <div class="container-fluid h-custom">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-md-9 col-lg-6 col-xl-5">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            class="img-fluid" alt="Sample image">
+        </div>
+        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+          <form action="{{ route('login-user') }}" method="POST">
+              @if(Session::has('sukses')):
+              <div class="alert alert success">{{ Session::get('sukses') }}</div>
+              @endif
+              @if(Session::has('gagal')):
+              <div class="alert alert success">{{ Session::get('gagal') }}</div>
+              @endif
+              @csrf
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+              <input type="email" id="form3Example3" class="form-control form-control-lg"
+                placeholder="Enter a valid email address" />
+              <label class="form-label" for="form3Example3">Email address</label>
+            </div>
+
+            <!-- Password input -->
+            <div class="form-outline mb-3">
+              <input type="password" id="form3Example4" class="form-control form-control-lg"
+                placeholder="Enter password" />
+              <label class="form-label" for="form3Example4">Password</label>
+            </div>
+
+            <div class="d-flex justify-content-between align-items-center">
+              <!-- Checkbox -->
+              <div class="form-check mb-0">
+                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                <label class="form-check-label" for="form2Example3">
+                  Remember me
+                </label>
+              </div>
+              <a href="#!" class="text-body">Forgot password?</a>
+            </div>
+
+            <div class="text-center text-lg-start mt-4 pt-2">
+              <button type="submit" class="btn btn-primary btn-lg"
+                style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+              <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+                  class="link-danger">Register</a></p>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
 
 </body>
 </html>
